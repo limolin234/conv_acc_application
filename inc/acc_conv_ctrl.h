af6de0adbb124d89122cef6535ee08c23008ee25
limolin234 <limolin234@gmail.com>
2026-04-08 16:59:58 +0800

    ?

--- inc/acc_conv_ctrl.h
+++ inc/acc_conv_ctrl.h
@@ -1,1 +1,15 @@
-#ifdef
+#ifndef lml_acc_conv
+#define lml_acc_conv
+
+#include<stdint.h>
+#include<cassert>
+
+struct alignas(16) instruction{
+    uint64_t l;
+    uint64_t h;
+    instruction(){
+    
+    }
+}
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,73 +1,40 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <sys/ioctl.h>
 #include <sys/mman.h>
-#include "tlsf.h"
 
-
-#define MYCTRL_IOC_MAGIC 'M'
-#define MYCTRL_IOC_GET_DMA_PHYS _IOR(MYCTRL_IOC_MAGIC, 0, uint64_t)
-#define MYCTRL_IOC_GET_DMA_SIZE _IOR(MYCTRL_IOC_MAGIC, 1, uint32_t)
-#define MYCTRL_IOC_GET_REG_PHYS _IOR(MYCTRL_IOC_MAGIC, 2, uint64_t)
-#define MYCTRL_IOC_GET_REG_SIZE _IOR(MYCTRL_IOC_MAGIC, 3, uint32_t)
-
-typedef struct{
-    uint64_t l;
-    uint64_t h;
-}instruction;
+#define EXCH_PHYS_ADDR  0x30000000ULL
+#define EXCH_SIZE       0x10000000UL
 
 int main(void)
 {
     int fd;
-    long page_size;
-    uint64_t reg_phys = 0, dma_phys = 0;
-    uint32_t reg_size = 0, dma_size = 0;
-    void *reg_map, *dma_map;
-    volatile uint32_t *reg;
+    void *exch_map;
 
-    fd = open("/dev/myctrl0", O_RDWR | O_SYNC);
+    fd = open("/dev/mem", O_RDWR | O_SYNC);
     if (fd < 0) {
-        perror("open");
+        perror("open /dev/mem");
         return 1;
     }
 
-    ioctl(fd, MYCTRL_IOC_GET_REG_PHYS, &reg_phys);
-    ioctl(fd, MYCTRL_IOC_GET_REG_SIZE, &reg_size);
-    ioctl(fd, MYCTRL_IOC_GET_DMA_PHYS, &dma_phys);
-    ioctl(fd, MYCTRL_IOC_GET_DMA_SIZE, &dma_size);
-
-    printf("reg phys = 0x%llx size = 0x%x\n", (unsigned long long)reg_phys, reg_size);
-    printf("dma phys = 0x%llx size = 0x%x\n", (unsigned long long)dma_phys, dma_size);
-
-    page_size = sysconf(_SC_PAGESIZE);
-
-    reg_map = mmap(NULL, reg_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0 * page_size);
-    dma_map = mmap(NULL, dma_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 256 * page_size);
-
-    if (reg_map == MAP_FAILED) {
-        perror("mmap reg");
+    exch_map = mmap(NULL, EXCH_SIZE,
+                    PROT_READ | PROT_WRITE,
+                    MAP_SHARED,
+                    fd,
+                    EXCH_PHYS_ADDR);
+    if (exch_map == MAP_FAILED) {
+        perror("mmap exchange");
         close(fd);
         return 1;
     }
 
-    if (dma_map == MAP_FAILED) {
-        perror("mmap dma");
-        munmap(reg_map, reg_size);
-        close(fd);
-        return 1;
-    }
-    reg = (uint32_t*)reg_map;
+    printf("exchange map = %p\n", exch_map);
 
-    tlsf_t pool = tlsf_create_with_pool(dma_map,dma_size);
-    instruction* instructions = tlsf_memalign(pool,64,128/8*512);
-    reg[1] = (uint32_t)instructions;
-    reg[2] = 1;
-    instructions[0].l = 0x4ULL;
-    
-    munmap(dma_map, dma_size);
-    munmap(reg_map, reg_size);
+    tlsf_t pool = tlsf_create_with_pool(exch_map, EXCH_SIZE);
+
+    munmap(exch_map, EXCH_SIZE);
     close(fd);
     return 0;
 }
